2e048ed2a078a513ab0422393c5427c9d91d50ec
yinpinghua <ph.yin@yunzhihulian.cn>
2020-01-13 13:34:17 +0800

    新增测试代码

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -3821,9 +3821,101 @@
 //	//}
 //}
 
+#include <locale>
 #include <iostream>
+#include <codecvt>
+#include <string>
+#pragma warning(disable:4996)
+std::wstring string_to_wstring(const std::string& str)
+{
+	setlocale(LC_ALL, "");
+	std::int64_t size = mbstowcs(NULL, str.c_str(), 0);
+	std::wstring w_str;
+	w_str.resize(size);
 
+	//算出代转wstring字节
+	mbstowcs(w_str.data(), str.c_str(), str.size());
+	return w_str;
+}
+
+std::string wstring_u8string(const std::wstring& wstr)
+{
+	std::wstring_convert<std::codecvt_utf8<wchar_t>>con;
+	return con.to_bytes(wstr);
+}
+
+bool is_valid_utf8(std::string str)
+{
+	if (str.empty())
+	{
+		return false;
+	}
+
+	const unsigned char* bytes = (const unsigned char*)str.c_str();
+	unsigned int cp;
+	int num;
+
+	while (*bytes != 0x00) {
+		if ((*bytes & 0x80) == 0x00) {
+			// U+0000 to U+007F 
+			cp = (*bytes & 0x7F);
+			num = 1;
+		}
+		else if ((*bytes & 0xE0) == 0xC0) {
+			// U+0080 to U+07FF 
+			cp = (*bytes & 0x1F);
+			num = 2;
+		}
+		else if ((*bytes & 0xF0) == 0xE0) {
+			// U+0800 to U+FFFF 
+			cp = (*bytes & 0x0F);
+			num = 3;
+		}
+		else if ((*bytes & 0xF8) == 0xF0) {
+			// U+10000 to U+10FFFF 
+			cp = (*bytes & 0x07);
+			num = 4;
+		}else {
+			return false;
+		}
+	
+		bytes += 1;
+		for (int i = 1; i < num; ++i) {
+			if ((*bytes & 0xC0) != 0x80)
+			{
+				return false;
+			}
+				
+			cp = (cp << 6) | (*bytes & 0x3F);
+			bytes += 1;
+		}
+
+		if ((cp > 0x10FFFF) ||
+			((cp >= 0xD800) && (cp <= 0xDFFF)) ||
+			((cp <= 0x007F) && (num != 1)) ||
+			((cp >= 0x0080) && (cp <= 0x07FF) && (num != 2)) ||
+			((cp >= 0x0800) && (cp <= 0xFFFF) && (num != 3)) ||
+			((cp >= 0x10000) && (cp <= 0x1FFFFF) && (num != 4)))
+		{
+			return false;
+		}
+			
+	}
+
+	return true;
+}
 int main()
 {
+	std::string str = "E:/素材/2016模型vray3.6稍微大点的场景/BaiduNetdiskDownload/模型/新中式风格/6/6/5/maps/欧模网_0232.jpg";
+	std::wstring wstr = string_to_wstring(str);
 
+	std::string utf8Str = wstring_u8string(wstr);
+
+	if (is_valid_utf8(utf8Str)){
+		int i = 100;
+	}
+
+	if (is_valid_utf8(str)) {
+		int i = 100;
+	}
 }
